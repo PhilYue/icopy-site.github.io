3bf3ce970e2587311ae41d2935a066eaaf86e6b5
Travis Builder <chenjiajia1@gmail.com>
2019-08-16 10:43:16 +0000

    Deployed 20baa47 with MkDocs version: 1.0.4

--- _static/js/extra.js
+++ _static/js/extra.js
@@ -39,4 +39,14 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(hm, s);
 })();
 
+var _mtac = {};
+(function() {
+    var mta = document.createElement("script");
+    mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+    mta.setAttribute("name", "MTAH5");
+    mta.setAttribute("sid", "500683764");
+
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(mta, s);
+})();
 
